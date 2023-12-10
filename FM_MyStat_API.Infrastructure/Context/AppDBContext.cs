1688d3b041607d35622bfad269874e11f9d1e536
SerhiyJI <serhiyji@gmail.com>
2023-12-10 19:23:35 +0200

    Add classes in FM_MyStat_API.Infrastructure

---
+++ FM_MyStat_API.Infrastructure/Context/AppDBContext.cs
@@ -0,0 +1,115 @@
+﻿using FM_MyStat_API.Core.Entities;
+using FM_MyStat_API.Core.Entities.Homeworks;
+using FM_MyStat_API.Core.Entities.Lessons;
+using FM_MyStat_API.Core.Entities.Users;
+using FM_MyStat_API.Infrastructure.Initializers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Org.BouncyCastle.Utilities.IO.Pem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM_MyStat_API.Infrastructure.Context
+{
+    internal class AppDBContext : IdentityDbContext
+    {
+        public AppDBContext() : base() { }
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Administrator> Administrators { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Group> Groups { get; set; } 
+        public DbSet<Homework> Homeworks { get; set; }
+        public DbSet<HomeworkDone> HomeworksDone { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<LessonMark> LessonMarks { get; set; }
+        public DbSet<TeacherSubject> TeachersSubjects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            // Links
+            modelBuilder.Entity<AppUser>()
+                .HasOne(user => user.Student).WithOne(student => student.AppUser)
+                .HasForeignKey<Student>(student => student.AppUserId).OnDelete(DeleteBehavior.Restrict);
+            
+            modelBuilder.Entity<AppUser>()
+                .HasOne(user => user.Teacher).WithOne(teacher => teacher.AppUser)
+                .HasForeignKey<Teacher>(teacher => teacher.AppUserId).OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<AppUser>()
+                .HasOne(user => user.Administrator).WithOne(administrator => administrator.AppUser)
+                .HasForeignKey<Administrator>(administrator => administrator.AppUserId).OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Student>()
+                .HasOne(student => student.Group).WithMany(group => group.Students)
+                .HasForeignKey(student => student.GroupId).OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Student>()
+                .HasMany(student => student.HomeworksDone).WithOne(homework => homework.Student)
+                .HasForeignKey(homework => homework.StudentId).OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Student>()
+                .HasMany(student => student.LessonMarks).WithOne(lessonmark => lessonmark.Student)
+                .HasForeignKey(lessonmark => lessonmark.StudentId).OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Group>()
+                .HasOne(group => group.Teacher).WithMany(teacher => teacher.Groups)
+                .HasForeignKey(group => group.TeacherId);
+
+            modelBuilder.Entity<Group>()
+                .HasMany(group => group.Homeworks).WithOne(homework => homework.Group)
+                .HasForeignKey(homework => homework.GroupId);
+
+            modelBuilder.Entity<Group>()
+                .HasMany(group => group.Lessons).WithOne(lesson => lesson.Group)
+                .HasForeignKey(lesson => lesson.GroupId);
+
+            modelBuilder.Entity<Homework>()
+                .HasMany(homework => homework.HomeworksDone).WithOne(homeworkdone => homeworkdone.Homework)
+                .HasForeignKey(homeworkdone => homeworkdone.HomeworkId);
+
+            modelBuilder.Entity<Lesson>()
+                .HasMany(lesson => lesson.LessonMarks).WithOne(lessonmark => lessonmark.Lesson)
+                .HasForeignKey(lessonmark => lessonmark.LessonId);
+
+            modelBuilder.Entity<Lesson>()
+                .HasOne(lesson => lesson.Teacher).WithMany(teacher => teacher.Lessons)
+                .HasForeignKey(lesson => lesson.TeacherId).OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Lesson>()
+                .HasOne(lesson => lesson.Homework).WithOne(homework => homework.Lesson)
+                .HasForeignKey<Lesson>(lesson => lesson.HomeworkId).OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Lesson>()
+                .HasOne(lesson => lesson.Subject).WithMany(subject => subject.Lessons)
+                .HasForeignKey(lesson => lesson.SubjectId);
+
+            modelBuilder.Entity<TeacherSubject>()
+                .HasOne(teachersubject => teachersubject.Teacher).WithMany(teacher => teacher.TeachersSubjects)
+                .HasForeignKey(teachersubject => teachersubject.TeacherId);
+
+            modelBuilder.Entity<TeacherSubject>()
+                .HasOne(teachersubject => teachersubject.Subject).WithMany(subject => subject.TeachersSubjects)
+                .HasForeignKey(teachersubject => teachersubject.SubjectId);
+
+            modelBuilder.SeedAdministrator();
+            modelBuilder.SeedTeacher();
+            modelBuilder.SeedGroup();
+            modelBuilder.SeedStudent();
+            modelBuilder.SeedSubject();
+            modelBuilder.SeedLesson();
+            modelBuilder.SeedTeacherSubject();
+        }
+    }
+}
---
+++ FM_MyStat_API.Infrastructure/Initializers/DBInitializer.cs
@@ -0,0 +1,185 @@
+﻿using FM_MyStat_API.Core.Entities;
+using FM_MyStat_API.Core.Entities.Lessons;
+using FM_MyStat_API.Core.Entities.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Group = FM_MyStat_API.Core.Entities.Group;
+
+namespace FM_MyStat_API.Infrastructure.Initializers
+{
+    public static class DBInitializer
+    {
+
+        public static void SeedAdministrator(this ModelBuilder modelBuilder)
+        {
+            var passwordHasher = new PasswordHasher<AppUser>();
+            var adminUserId = Guid.NewGuid().ToString();
+            var adminRoleId = Guid.NewGuid().ToString();
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = adminRoleId,
+                Name = "Administrator",
+                NormalizedName = "ADMINISTRATOR"
+            });
+            var adminUser = new AppUser
+            {
+                Id = adminUserId,
+                FirstName = "John",
+                LastName = "Connor",
+                SurName = "Johnovych",
+                UserName = "admin@email.com",
+                NormalizedUserName = "ADMIN@EMAIL.COM",
+                Email = "admin@email.com",
+                NormalizedEmail = "ADMIN@EMAIL.COM",
+                EmailConfirmed = true,
+                PhoneNumber = "+xx(xxx)xxx-xx-xx",
+                PhoneNumberConfirmed = true,
+                AdministratorId = 1,
+            };
+            Administrator administrator = new Administrator
+            {
+                Id = 1,
+                AppUserId = adminUserId
+            };
+            adminUser.PasswordHash = passwordHasher.HashPassword(adminUser, "Qwerty-1");
+            modelBuilder.Entity<Administrator>().HasData(administrator);
+            modelBuilder.Entity<AppUser>().HasData(adminUser);
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = adminRoleId,
+                UserId = adminUserId
+            });
+        }
+        public static void SeedTeacher(this ModelBuilder modelBuilder)
+        {
+            var passwordHasher = new PasswordHasher<AppUser>();
+            var teacherUserId = Guid.NewGuid().ToString();
+            var teacherRoleId = Guid.NewGuid().ToString();
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = teacherRoleId,
+                Name = "Teacher",
+                NormalizedName = "TEACHER"
+            });
+            var teacherUser = new AppUser
+            {
+                Id = teacherUserId,
+                FirstName = "John",
+                LastName = "Connor",
+                SurName = "Johnovych",
+                UserName = "teacher@email.com",
+                NormalizedUserName = "TEACHER@EMAIL.COM",
+                Email = "teacher@email.com",
+                NormalizedEmail = "TEACHER@EMAIL.COM",
+                EmailConfirmed = true,
+                PhoneNumber = "+xx(xxx)xxx-xx-xx",
+                PhoneNumberConfirmed = true,
+                TeacherId = 1,
+            };
+            Teacher teacher = new Teacher
+            {
+                Id = 1,
+                AppUserId = teacherUserId
+            };
+            teacherUser.PasswordHash = passwordHasher.HashPassword(teacherUser, "Qwerty-1");
+            modelBuilder.Entity<Teacher>().HasData(teacher);
+            modelBuilder.Entity<AppUser>().HasData(teacherUser);
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = teacherRoleId,
+                UserId = teacherUserId
+            });
+        }
+        public static void SeedStudent(this ModelBuilder modelBuilder)
+        {
+            var passwordHasher = new PasswordHasher<AppUser>();
+            var studentUserId = Guid.NewGuid().ToString();
+            var studentRoleId = Guid.NewGuid().ToString();
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = studentRoleId,
+                Name = "Student",
+                NormalizedName = "STUDENT"
+            });
+            var studentUser = new AppUser
+            {
+                Id = studentUserId,
+                FirstName = "John",
+                LastName = "Connor",
+                SurName = "Johnovych",
+                UserName = "student@email.com",
+                NormalizedUserName = "STUDENT@EMAIL.COM",
+                Email = "student@email.com",
+                NormalizedEmail = "STUDENT@EMAIL.COM",
+                EmailConfirmed = true,
+                PhoneNumber = "+xx(xxx)xxx-xx-xx",
+                PhoneNumberConfirmed = true,
+                StudentId = 1,
+            };
+            Student student = new Student
+            {
+                Id = 1,
+                GroupId = 1,
+                AppUserId = studentUserId
+            };
+            studentUser.PasswordHash = passwordHasher.HashPassword(studentUser, "Qwerty-1");
+            modelBuilder.Entity<Student>().HasData(student);
+            modelBuilder.Entity<AppUser>().HasData(studentUser);
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = studentRoleId,
+                UserId = studentUserId
+            });
+        }
+        public static void SeedSubject(this ModelBuilder modelBuilder)
+        {
+            Subject subject = new Subject
+            {
+                Id = 1,
+                Name = "C#"
+            };
+            modelBuilder.Entity<Subject>().HasData(subject);
+        }
+        public static void SeedLesson(this ModelBuilder modelBuilder)
+        {
+            Lesson lesson = new Lesson
+            {
+                Id = 1,
+                Name = "C# beginning",
+                Description = "First steps into c# today",
+                Start = DateTime.UtcNow.AddHours(1),
+                End = DateTime.UtcNow.AddHours(3),
+                TeacherId = 1,
+                GroupId = 1,
+                SubjectId = 1
+            };
+            modelBuilder.Entity<Lesson>().HasData(lesson);
+        }
+        public static void SeedGroup(this ModelBuilder modelBuilder)
+        {
+            Group group = new Group
+            {
+                Id = 1,
+                Name = "suicide terrorists",
+                TeacherId = 1
+            };
+            modelBuilder.Entity<Group>().HasData(group);
+        }
+        public static void SeedTeacherSubject(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TeacherSubject>().HasData(new TeacherSubject()
+            {
+                Id = 1,
+                SubjectId = 1,
+                TeacherId = 1
+            });
+        }
+    }
+}
