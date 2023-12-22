588d86dee6641597bf5d47d0f9848c66848b700c
SerhiyJI <serhiyji@gmail.com>
2023-12-22 15:15:58 +0200

    Create empty controllers

---
+++ FM_MyStat_API.Api/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FM_MyStat_API.Api.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FM_MyStat_API.Api/Controllers/TeacherSubjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FM_MyStat_API.Api.Controllers
+{
+    public class TeacherSubjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
